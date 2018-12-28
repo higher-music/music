d11b39de8a1efe4e9e69708698cf3088a6e16f2e
OSganping <osganping@outlook.com>
2018-12-28 16:35:48 +0800

    去除element无用代码

--- src/main.js
+++ src/main.js
@@ -4,19 +4,12 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css';
 
 // import store from './vuex/store'
 
 import * as filters from './util/filter'
 
-
 import './icons'
-
-Vue.use(Element, {
-  size: 'small'
-})
 
 Vue.config.productionTip = false
 
