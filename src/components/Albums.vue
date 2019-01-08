c6e3785d5e325bcf150f01a536272ecf595181a4
osganping <osganping@gmail.com>
2019-01-08 23:21:00 +0800

    专辑页开发

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -2,10 +2,17 @@
   <div class="albums-container">
     <header>
       <div class="header-image-container">
-        <img class="header-image" src="" alt="">
+        <img :src="headerInfo.pic_album" :alt="headerInfo.ListName" class="header-image">
       </div>
       <div class="header">
-        <div class="artwork" style=""/>
+        <div :style="{backgroundImage:`url(${headerInfo.pic_album})`}" class="artwork"/>
+        <div class="album-extras">
+          <div class="track-text">
+            <span class="album-name">{{ headerInfo.ListName }}</span>
+            <span class="album-detail" v-html="headerInfo.info"/>
+          </div>
+          <div class="album-button-container"/>
+        </div>
       </div>
     </header>
     <section>
@@ -20,10 +27,12 @@
 import { createSong } from '@/components/js/song';
 import { getMusicList } from '@/api/rank'
 import SongList from '@/components/SongList'
+
 export default {
   components: { SongList },
-  data(){
+  data() {
     return {
+      headerInfo: [],
       songList: []
     }
   },
@@ -39,8 +48,10 @@ export default {
     }
   },
   methods: {
-    getRankList(data){
+    getRankList(data) {
       getMusicList(data.params.id).then(res => {
+        this.headerInfo = res.topinfo
+        console.log(this.headerInfo)
         const songs = []
         const songList = (res.songlist).slice(0, 100)
         songList.forEach((item) => {
@@ -55,7 +66,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .albums-container{
+  .albums-container {
     background-color: #000;
     height: 100%;
     overflow-x: hidden;
@@ -67,7 +78,7 @@ export default {
     transform-style: preserve-3d;
     -webkit-animation: .7s fadein;
     animation: .7s fadein;
-    header{
+    header {
       min-height: 190px;
       max-height: 300px;
       height: 50%;
@@ -77,13 +88,13 @@ export default {
       transform-origin: center top;
       -webkit-transform: translateZ(-.6px) scale(1.6);
       transform: translateZ(-.6px) scale(1.6);
-      .header-image-container{
+      .header-image-container {
         width: 100%;
         height: 100%;
         overflow: hidden;
         position: absolute;
         background-color: #000;
-        .header-image{
+        .header-image {
           width: 100%;
           height: 100%;
           -o-object-position: center 30%;
@@ -93,28 +104,49 @@ export default {
           opacity: .45;
         }
       }
-      .header{
+      .header {
         position: absolute;
         height: 100%;
         display: flex;
         align-items: flex-end;
-        .artwork{
+        .artwork {
           background-size: cover;
           min-width: 170px;
           height: 170px;
           margin: 10px;
-          box-shadow: 0 0 10px rgba(0,0,0,.5);
+          box-shadow: 0 0 10px rgba(0, 0, 0, .5);
           border-radius: 4px;
         }
+      .album-extras {
+        display: flex;
+        flex-direction: column;
+        padding-bottom: 10px;
+        margin-left: 5px;
+        .track-text{
+          line-height: normal;
+          display: flex;
+          flex-direction: column;
+          padding-bottom: 12px;
+          cursor: default;
+          .album-name{
+            font-weight: 500;
+            font-size: 38px;
+          }
+          .album-detail{
+            color: #b3b3b3;
+            padding-top: 3px;
+          }
+        }
+      }
       }
     }
-    section{
+    section {
       box-shadow: 0 0 20px 0 #000;
-      background-image: linear-gradient(#04060c,#1e3264 120%);
+      background-image: linear-gradient(#04060c, #1e3264 120%);
       -webkit-transform: translateZ(0);
       transform: translateZ(0);
       min-height: calc(100% - 300px);
-      .tracklist{
+      .tracklist {
         padding: 10px;
       }
     }
