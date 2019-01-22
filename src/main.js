19cfc90aeb0e05fb43bcd3a7c9f3514ad5d57d7f
ganp1020 <ganp1020@outlook.com>
2019-01-22 12:28:06 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Raven from 'raven-js'
-import RavenVue from 'raven-js/plugins/vue'
+// import Raven from 'raven-js'
+// import RavenVue from 'raven-js/plugins/vue'
 import Vuetify from 'vuetify'
 import store from './vuex/store'
 import '@/style/index.scss'
@@ -13,9 +13,9 @@ import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 Vue.use(Vuetify)
 Vue.config.productionTip = false;
 
-window.$Raven = Raven
-Raven.config('https://2eff0ea26f4041ccaf115a0a52e50d4c@sentry.io/1376437')
-  .addPlugin(RavenVue, Vue).install()
+// window.$Raven = Raven
+// Raven.config('https://2eff0ea26f4041ccaf115a0a52e50d4c@sentry.io/1376437')
+//   .addPlugin(RavenVue, Vue).install()
 
 /* eslint-disable no-new */
 new Vue({
