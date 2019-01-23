bb2b93bc6998952a10f4021a39e0796b171024ac
cyixlq <2421597157@qq.com>
2019-01-23 10:30:55 +0800

    修改search页面细节

--- src/pages/search.vue
+++ src/pages/search.vue
@@ -3,9 +3,9 @@
     <form action="javascript:getSearchList">
       <v-text-field v-model="text" label="Search" color="#7b1fa2" clearable @keydown.enter="getSearchList"/>
     </form>
-    <div class="hotkey">
+    <div v-if="showHotKeys" class="scroll-container" style="padding-bottom: 5px">
       <span style="font-size: large;font-weight: bolder">HotKeys:</span>
-      <v-chip v-for="(item, index) in hotkeys" :key="index" outline text-color="#fff" @click="searchHotKey(item.k)">{{ item.k }}</v-chip>
+      <v-chip v-for="(item, index) in hotKeys" :key="index" outline text-color="#fff" @click="searchHotKey(item.k)">{{ item.k }}</v-chip>
     </div>
     <div class="scroll-container">
       <SongList :data="songList" :loading="loading"/>
@@ -26,16 +26,26 @@ export default {
       text: '',
       loading: false,
       songList: [],
-      hotkeys: []
+      hotKeys: [],
+      showHotKeys: true
+    }
+  },
+  watch: {
+    text() {
+      if (!this.text) {
+        this.showHotKeys = true
+        this.songList = []
+      }
     }
   },
   created() {
     getHotKey().then((res) => {
-      this.hotkeys = res.data.hotkey
+      this.hotKeys = res.data.hotkey
     })
   },
   methods: {
     getSearchList(){
+      this.showHotKeys = false
       this.loading = true
       const songs = []
       search(this.text, 1, 50).then((res) => {
@@ -48,19 +58,12 @@ export default {
         }, 1000)
       })
     },
-    searchHotKey(hotkey) {
-      this.text = hotkey
+    searchHotKey(hotKey) {
+      this.text = hotKey
       this.getSearchList()
     }
   }
 }
 </script>
 <style lang="scss" scoped>
-  .hotkey {
-    display: flex;
-    display: -moz-flex;
-    display: -webkit-flex;
-    flex-wrap: wrap;
-    align-items: center;
-  }
 </style>
