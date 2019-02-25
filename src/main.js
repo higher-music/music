590155c6af32934f02036760865e66bdfb933591
ganp1020 <ganp1020@outlook.com>
2019-02-25 21:01:20 +0800

    提高lazy加载图片的性能

--- src/main.js
+++ src/main.js
@@ -26,7 +26,13 @@ Vue.use(Scroll)
 Vue.use(VueLazyload, {
   error: 'static/img/default.jpeg', // 图像的加载失败时 显示的error图标
   loading: 'static/img/default.jpeg', // 图像正常加载时 显示的loading图标
-  attempt: 2 // 图像尝试加载 次数
+  attempt: 2, // 图像尝试加载 次数
+  observer: true,
+  // optional
+  observerOptions: {
+    rootMargin: '0px',
+    threshold: 0.1
+  }
 })
 
 // 是否是原生Safari
