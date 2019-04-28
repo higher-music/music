896476025e365ca5ea85423d05929ecec3092caa
ganp1020 <ganp1020@outlook.com>
2019-04-28 09:53:52 +0800

    delect

--- src/plugins/registration.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Loading from '@/components/Loading'
-import Scroll from '@/components/Scroll'
-import noResources from '@/components/noResources'
-
-
-Vue.use(Loading)
-Vue.use(noResources)
-Vue.use(Scroll)
