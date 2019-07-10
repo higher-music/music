4f676ea41f0f26b363559e8961875ae53e90f4b7
ganp1020 <ganp1020@outlook.com>
2019-07-10 20:53:39 +0800

    添加分析工具 添加动态polyfill

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
 
 function resolve(dir) {
-    return path.join(__dirname, dir)
+  return path.join(__dirname, dir)
 }
 
 module.exports = {
-    productionSourceMap: false,
-    publicPath: './',
-    chainWebpack: (config) => {
-        config.resolve.alias
-            .set('static', resolve('public/static'))
+  productionSourceMap: false,
+  publicPath: './',
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('static', resolve('public/static'))
+    /* 添加分析工具*/
+    if (process.env.NODE_ENV === 'production') {
+      if (process.env.npm_config_report) {
+        config
+          .plugin('webpack-bundle-analyzer')
+          .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+          .end();
+        config.plugins.delete('prefetch')
+      }
     }
+  }
 };
